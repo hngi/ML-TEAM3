b56559c045602a84cba6b08593c614414ab9e80e
Omobolaji <balogunbolaji61@gmail.com>
2019-10-26 12:46:50 +0100

    Update address.py

--- address.py
+++ address.py
@@ -7,14 +7,22 @@ dataframe = pd.read_csv('isthisarealjob_table_posts.csv')
 dataframe = dataframe.drop(['3', '4', '2018-02-28 00:14:37', '2018-02-28 00:14:37.1', 'hello-world-071235157V21576', 'world,good', 'Unnamed: 9', 'NULL', 'NULL.1'], axis= 1)
 dataframe = dataframe[['danny', '#world is greater than #good things', 'Hello World']]
 dataframe_address = dataframe['#world is greater than #good things']
-dataframe_address = dataframe_address.dropna()
 
-#checking if the address actually exist
-for address in dataframe_address:
-    # Geocoding an address
-    geocode_result = gmaps.geocode(address)
-    if geocode_result != '[]':
-        print('1')
+
+#input an address
+address= input("Enter address: ")
+
+
+# Geocoding an address
+geocode_result = gmaps.geocode(address)
+
+if geocode_result == []:
+    print ("This address is invalid")
+else:
+    geocode_result= geocode_result[0]
+    
+    if 'plus_code' in geocode_result:
+        print("The Company address is valid")
     else:
-        print('0')
-    # print(geocode_result)
+        print("This address is vague, This job invite is likely a scam")
+
